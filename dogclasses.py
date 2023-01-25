525ac75fc45afcd3e2d455dce919c66e41050cf5
Paul Heller <paulleonardoheller@yahoo.de>
2023-01-25 13:12:57 +0100

    Added dog

--- dogclasses.py
+++ dogclasses.py
@@ -1,2 +1,51 @@
+from abc import ABC, abstractmethod
+
+# abstract class
+class AbstractClassExample(ABC):
+
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+    @abstractmethod
+    def do_something(self):
+        pass
+class concreteClass(AbstractClassExample):
+    def __init__(self, value, zahl):
+        super().__init__(value)
+        self.zahl = zahl
+
+    def do_something(self):
+        return self.value * self.zahl
+
+
+
 class dog:
-    pass
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    def speak(self, sound):
+        return f"{self.name} says {sound}"
+    def speak2(self):
+        print('wau wau')
+    def addthings(self, a, b):
+        self.a=a
+        self.b=b
+        c = self.a + self.b
+        return c
+    def returningab(self):
+        return self.a, self.b
+
+class JackRussellTerrier(dog):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+    def speak2(self):
+        print('wau wau wau')
+
+
+
+
+
+
+
--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 
-from dogclasses import dog
+from dogclasses import dog, JackRussellTerrier, concreteClass
 import numpy as np
 
 
@@ -7,8 +7,17 @@ import numpy as np
 def main():
     a = np.arange(0, 20)
     print('a', a)
+    pinky = dog('pinky', 10)
+    print(pinky.age, pinky.name)
+    pinky.speak2()
+    print(pinky.addthings(a=2, b=3))
+    print(pinky.returningab())
+    jacky = JackRussellTerrier('jacky', 2)
+    jacky.speak2()
 
 
+    cnc = concreteClass(value=3, zahl=-4)
+    print('do smth', cnc.do_something())
 
 if __name__ == "__main__":
     main()
